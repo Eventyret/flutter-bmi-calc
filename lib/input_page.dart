602372a950328172629405daed567ee6462cab1a
Simen Daehlin <simen.dehlin@gmail.com>
2019-05-23 10:00:05 +0100

    Card gesture trigger works need refactoring

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -23,6 +23,7 @@ class _InputPageState extends State<InputPage> {
     if (gender == 1) {
       if (maleCardColor == inactiveCardColor) {
         maleCardColor = activeCardColor;
+        femaleCardColor = inactiveCardColor;
       } else {
         maleCardColor = inactiveCardColor;
       }
@@ -30,6 +31,7 @@ class _InputPageState extends State<InputPage> {
     if (gender == 2) {
       if (femaleCardColor == inactiveCardColor) {
         femaleCardColor = activeCardColor;
+        maleCardColor = inactiveCardColor;
       } else {
         femaleCardColor = inactiveCardColor;
       }
